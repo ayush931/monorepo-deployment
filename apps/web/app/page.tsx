beb8d211475950f1ea3f84cf7649548f734cc2af
ayush931 <ayushkumar9315983@gmail.com>
2026-04-10 12:36:32 +0530

    Changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
 import { prismaClient } from '@repo/database/client'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const res = await prismaClient.user.findMany();
 
